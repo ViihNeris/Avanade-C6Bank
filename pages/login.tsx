50681432ad309282a3f926829a1a72039c493f2b
Viih Neris <viihneris049@gmail.com>
2025-02-13 00:06:26 -0300

    FIX: Remove unnecessary imports

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,15 +1,5 @@
 import React, { useEffect, useState, FormEvent } from "react";
-import {
-  Typography,
-  Container,
-  Button,
-  TextField,
-  Checkbox,
-  Box,
-  FormControlLabel,
-  CssBaseline,
-  Stack,
-} from "@mui/material";
+import { Container, CssBaseline } from "@mui/material";
 import MuiAlert, { AlertProps } from "@mui/material/Alert";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Copyright from "../components/utils/Copyright/";
